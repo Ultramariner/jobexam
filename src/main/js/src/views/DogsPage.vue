1cb671678e9bd4edece46edc02b8846ccf7a704d
Ultramar <artyom.odinochenko@yandex.ru>
2024-08-01 17:33:09 +0300

    Localization fix

--- src/main/js/src/views/DogsPage.vue
+++ src/main/js/src/views/DogsPage.vue
@@ -11,45 +11,28 @@ let imgUrl = ref("blank.jpg");
 let dogName = ref(null);
 let dogComment = ref(null);
 let breed = ref(null);
-let breeds = ref([]);
+let breeds = ref();
 let breedsList = ref([]);
-const breedsMap = {};
 const imgLoaded = ref(false);
+const breedsMap = ref(new Map());
 
 onBeforeMount(async () => {
   try {
     const response = await axios.get('http://localhost:8080/jobexam/vue/dogs');
     const responseLocalization = await axios.get('http://localhost:8080/jobexam/vue/dogs/breeds/ru');
     breeds.value = Array.from(response.data);
-
-    // breeds = breedsMap;
-
-    // breeds = Array.from(response.data);
-    // console.log(breeds);
-    //
-    // for (let [key, value] of breeds) {
-    //   console.log(`Значение было: ${value}`);
-    //   value = responseLocalization[value];
-    //   console.log(`Значение стало: ${value}`);
-    // }
-
-    // breeds.forEach((breed) => {
-    //   const localizedValue = responseLocalization.data[breed.name] || breed.name;
-    //   breedsMap.set(breed.name, localizedValue);
-    // });
-
-    // breeds.value = breedsMap;
-
+    breeds.value.forEach((breed) => {
+      if (responseLocalization.data[breed.name]) {
+        breedsMap.value.set(breed.name, responseLocalization.data[breed.name]);
+      } else {
+        breedsMap.value.set(breed.name, breed.name);
+      }
+    });
   } catch (error) {
     console.error('Ошибка при получении данных:', error);
   }
 });
 
-// let breedsList = computed(() => {
-//   console.log('breedsList:');
-//   console.log(Object.values(breedsMap));
-//   return Object.values(breedsMap);
-// });
 
 const getImg = async () => {
   if (!breed.value) {
@@ -57,7 +40,7 @@ const getImg = async () => {
     return;
   }
   try {
-    const response = await axios.get(`http://localhost:8080/jobexam/vue/dogs/${breed.value.name}`);
+    const response = await axios.get(`http://localhost:8080/jobexam/vue/dogs/${breed.value.key}`);
     imgUrl.value = response.data;
   } catch (error) {
     console.error('Ошибка при получении изображения:', error);
@@ -72,7 +55,7 @@ const save = async () => {
   try {
     await axios.post(`http://localhost:8080/jobexam/vue/dogs`, {
       name: dogName.value,
-      breed: breed.value.name,
+      breed: breed.value.key,
       comment: dogComment.value,
       link: imgUrl.value,
     });
@@ -86,25 +69,17 @@ const handleImageLoad = () => {
     imgLoaded.value = true;
   }
 };
+
+const breedsOptions = computed(() => {
+  return Array.from(breedsMap.value).map(([key, value]) => ({ key: key, label: value }));
+});
 </script>
 
 <template>
   <div class="main-container">
     <div class="content-box">
       <form v-on:submit.prevent="formSubmit">
-<!--        <template v-if="breeds">-->
-<!--          <Dropdown v-model="breed" :options="breeds" optionLabel="name" placeholder="Выберите породу" />-->
-<!--        </template>-->
-<!--        <template v-else>-->
-<!--          Загрузка...-->
-<!--        </template>-->
-        <Dropdown v-model="breed" :options="breeds" optionLabel="name" @change="getImg" placeholder="Выберите породу" />
-<!--        <select v-model="breed" @change=getImg>-->
-<!--          <option value="" disabled selected>Выберите породу</option>-->
-<!--          <option v-for="breed in breeds">-->
-<!--            {{ breed.name }}-->
-<!--          </option>-->
-<!--        </select>-->
+        <Dropdown v-model="breed" :options="breedsOptions" optionLabel="label" @change="getImg" placeholder="Выберите породу" />
         <InputText v-if="imgLoaded" v-model="dogName" placeholder="Имя" />
         <InputText v-if="imgLoaded" v-model="dogComment" placeholder="Комментарий" />
         <hr/>
